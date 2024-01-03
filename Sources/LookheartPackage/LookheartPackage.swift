e13d28e9c6979dfa611d81f11b2e9c59cde37b25
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-03 10:10:16 +0900

    test

--- Sources/LookheartPackage/LookheartPackage.swift
+++ Sources/LookheartPackage/LookheartPackage.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct LookheartPackage {
+    public private(set) var text = "test"
+    
+    public init(){
+    }
+    
+    public func test() {
+        print("Test Completed!")
+    }
+    
+}
