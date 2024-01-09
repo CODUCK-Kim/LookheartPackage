5783526f94dc29e4a9d9df47efadb5c05a93a09c
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-09 15:37:09 +0900

    fix

--- Sources/LookheartPackage/Server/UserProfileManager.swift
+++ Sources/LookheartPackage/Server/UserProfileManager.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-open class UserProfileManager {
+public class UserProfileManager {
     
     public static let shared = UserProfileManager()
     
@@ -10,11 +10,11 @@ open class UserProfileManager {
     public init() { }
     
     // UserProfile
-    func setUserProfile(_ profile: UserProfile) {
+    public func setUserProfile(_ profile: UserProfile) {
         self.userProfile = profile
     }
     
-    func getUserProfile() -> UserProfile {
+    public func getUserProfile() -> UserProfile {
         return userProfile!
     }
     
@@ -22,136 +22,137 @@ open class UserProfileManager {
     // ---------------------------- PROFILE ---------------------------- //
     
     // Email
-    func getEmail() -> String {
+    public func getEmail() -> String {
         return userProfile?.eq ?? "isEmpty"
     }
     
     // name
-    func setName(_ name: String) {
+    public func setName(_ name: String) {
         userProfile?.eqname = name;
     }
-    func getName() -> String {
+    
+    public func getName() -> String {
         return userProfile?.eqname ?? "isEmpty"
     }
     
     // phone
-    func setPhoneNumber(_ phoneNumber: String) {
+    public func setPhoneNumber(_ phoneNumber: String) {
         userProfile?.userphone = phoneNumber;
     }
-    func getPhoneNumber() -> String {
+    public func getPhoneNumber() -> String {
         return userProfile?.userphone ?? "01012345678"
     }
     
     // birth
-    func setBirthDate(_ birthDate: String) {
+    public func setBirthDate(_ birthDate: String) {
         userProfile?.birth = birthDate;
     }
-    func getBirthDate() -> String {
+    public func getBirthDate() -> String {
         return userProfile?.birth ?? "isEmpty"
     }
     
     // age
-    func setAge(_ age: String) {
+    public func setAge(_ age: String) {
         userProfile?.age = age;
     }
-    func getAge() -> String {
+    public func getAge() -> String {
         return userProfile?.age ?? "isEmpty"
     }
     
     // gender
-    func setGender(_ gender: String) {
+    public func setGender(_ gender: String) {
         userProfile?.sex = gender;
     }
-    func getGender() -> String {
+    public func getGender() -> String {
         return userProfile?.sex ?? "isEmpty"
     }
     
     // height
-    func setHeight(_ height: String) {
+    public func setHeight(_ height: String) {
         userProfile?.height = height;
     }
-    func getHeight() -> String {
+    public func getHeight() -> String {
         return userProfile?.height ?? "isEmpty"
     }
     
     // weight
-    func setWeight(_ weight: String) {
+    public func setWeight(_ weight: String) {
         userProfile?.weight = weight;
     }
-    func getWeight() -> String {
+    public func getWeight() -> String {
         return userProfile?.weight ?? "isEmpty"
     }
     
     // sleep time
-    func setBedtime(_ bedtime: Int) {
+    public func setBedtime(_ bedtime: Int) {
         userProfile?.sleeptime = bedtime;
     }
-    func getBedtime() -> Int {
+    public func getBedtime() -> Int {
         return userProfile?.sleeptime ?? 23
     }
     
     // wake time
-    func setWakeUpTime(_ WakeUpTime: Int) {
+    public func setWakeUpTime(_ WakeUpTime: Int) {
         userProfile?.uptime = WakeUpTime;
     }
-    func getWakeUpTime() -> Int {
+    public func getWakeUpTime() -> Int {
         return userProfile?.uptime ?? 7
     }
     
-    func getJoinDate() -> String {
+    public func getJoinDate() -> String {
         return userProfile?.signupdate ?? "2023-01-01"
     }
     
     // ---------------------------- PROFILE ---------------------------- //
     
     // A.bpm
-    func setBpm(_ bpm: Int) {
+    public func setBpm(_ bpm: Int) {
         userProfile?.bpm = bpm;
     }
-    func getBpm() -> Int {
+    public func getBpm() -> Int {
         return userProfile?.bpm ?? 90
     }
     
     // step
-    func setStep(_ step: Int) {
+    public func setStep(_ step: Int) {
         userProfile?.step = step;
     }
-    func getStep() -> Int {
+    public func getStep() -> Int {
         return userProfile?.step ?? 2000
     }
     
     // distance
-    func setDistance(_ distance: Int) {
+    public func setDistance(_ distance: Int) {
         userProfile?.distanceKM = distance;
     }
-    func getDistance() -> Int {
+    public func getDistance() -> Int {
         return userProfile?.distanceKM ?? 5
     }
     
     // A.cal
-    func setACal(_ aCal: Int) {
+    public func setACal(_ aCal: Int) {
         userProfile?.calexe = aCal;
     }
-    func getACal() -> Int {
+    public func getACal() -> Int {
         return userProfile?.calexe ?? 500
     }
     
     // total cal
-    func setTCal(_ tCal: Int) {
+    public func setTCal(_ tCal: Int) {
         userProfile?.cal = tCal;
     }
-    func getTCal() -> Int {
+    public func getTCal() -> Int {
         return userProfile?.cal ?? 500
     }
     
     // ---------------------------- SETTING ---------------------------- //
     
     // guardianPhoneNumber
-    func setPhoneNumbers(_ numbers: [String]) {
+    public func setPhoneNumbers(_ numbers: [String]) {
         guardianPhoneNumbers = numbers
     }
     
-    func getPhoneNumbers() -> [String] {
+    public func getPhoneNumbers() -> [String] {
         return guardianPhoneNumbers
     }
 
