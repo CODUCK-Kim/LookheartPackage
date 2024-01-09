295670aa66acebc4aeb4a13c4d5a74e196aa81e0
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-09 14:41:24 +0900

    update

---
+++ Sources/LookheartPackage/Summary/PSummaryArr.swift
@@ -0,0 +1,8 @@
+//
+//  File 5.swift
+//  
+//
+//  Created by 정연호 on 2024/01/09.
+//
+
+import Foundation
---
+++ Sources/LookheartPackage/Summary/PSummaryHrv.swift
@@ -0,0 +1,8 @@
+//
+//  File 4.swift
+//  
+//
+//  Created by 정연호 on 2024/01/09.
+//
+
+import Foundation
