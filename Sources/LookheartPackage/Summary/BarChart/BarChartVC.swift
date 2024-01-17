e6f197e9943eb30e5b5f409f3f00c7e3c597cbec
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-17 16:50:45 +0900

    tt

--- Sources/LookheartPackage/Summary/BarChart/BarChartVC.swift
+++ Sources/LookheartPackage/Summary/BarChart/BarChartVC.swift
@@ -289,14 +289,14 @@ class BarChartVC : UIViewController {
         $0.text = "-"
         $0.textAlignment = .center
         $0.font = UIFont.systemFont(ofSize: 15, weight: .bold)
-        $0.textColor = .red
+        $0.textColor = .darkGray
     }
     
     private let bottomValueProcent = UILabel().then {
         $0.text = "-"
         $0.textAlignment = .center
         $0.font = UIFont.systemFont(ofSize: 15, weight: .bold)
-        $0.textColor = .blue
+        $0.textColor = .darkGray
     }
     
     private let bottomLine = UILabel().then {   $0.backgroundColor = .lightGray }
