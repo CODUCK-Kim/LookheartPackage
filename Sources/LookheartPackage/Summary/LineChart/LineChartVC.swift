415eef5bc3a5cedbbaaf4c62fc00edd2c5cd2d18
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-18 11:48:54 +0900

    test

--- Sources/LookheartPackage/Summary/LineChart/LineChartVC.swift
+++ Sources/LookheartPackage/Summary/LineChart/LineChartVC.swift
@@ -56,7 +56,7 @@ class LineChartVC : UIViewController {
         $0.style = UIActivityIndicatorView.Style.large
     }
     
-    private lazy var calendar = CustomCalendar(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 300)).then {
+    private lazy var calendar = CustomCalendar(frame: CGRect(x: 0, y: 0, width: 0, height: 0)).then {
         $0.isHidden = true
     }
     
