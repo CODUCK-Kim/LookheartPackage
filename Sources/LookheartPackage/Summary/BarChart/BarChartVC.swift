ed847d08f913b6908cca94651ad669e9c0c9e6f2
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-18 09:45:35 +0900

    test

--- Sources/LookheartPackage/Summary/BarChart/BarChartVC.swift
+++ Sources/LookheartPackage/Summary/BarChart/BarChartVC.swift
@@ -655,10 +655,10 @@ class BarChartVC : UIViewController {
             barChartView.xAxis.centerAxisLabelsEnabled = true
         default:
             
-            let defaultBarWidth = 0.85 // 기본 바 너비
+            let defaultBarWidth = 1.0 // 기본 바 너비
             chartData.barWidth = defaultBarWidth
 
-            barChartView.xAxis.axisMinimum = -defaultBarWidth / 2
+            barChartView.xAxis.axisMinimum = 0
             barChartView.xAxis.axisMaximum = Double(labelCnt)
             
             barChartView.xAxis.centerAxisLabelsEnabled = false
