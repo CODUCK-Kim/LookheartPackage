1e482af32cfa022c1be98c37c16569b7f86249a3
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-19 14:14:44 +0900

    test

--- Sources/LookheartPackage/Utility/Toast.swift
+++ Sources/LookheartPackage/Utility/Toast.swift
@@ -60,6 +60,50 @@ public class ToastHelper {
         })
     }
     
+    public func showToast(_ view: UIView, _ message: String) {
+            let toastContainer = UIView(frame: CGRect())
+            toastContainer.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+            toastContainer.layer.cornerRadius = 10
+            toastContainer.clipsToBounds = true
+            
+            let iconImageView = UIImageView(image: UIImage(named: "toast_icon")!)
+            let iconSize: CGFloat = 25
+            let iconPadding: CGFloat = 10
+            iconImageView.frame = CGRect(x: iconPadding, y: 5, width: iconSize, height: iconSize)
+            iconImageView.contentMode = .scaleAspectFit
+            
+            let toastLabel = UILabel()
+            toastLabel.textColor = UIColor.white
+            toastLabel.font = UIFont.systemFont(ofSize: 16.0)
+            toastLabel.textAlignment = .center
+            toastLabel.text = message
+            toastLabel.numberOfLines = 0
+
+            toastContainer.addSubview(iconImageView)
+            toastContainer.addSubview(toastLabel)
+            
+            // 토스트 컨테이너 크기 조정
+            let containerWidth: CGFloat = view.frame.width - 60
+            let containerHeight: CGFloat = 35
+            toastContainer.frame = CGRect(x: (view.frame.size.width - containerWidth) / 2,
+                                          y: setToastLocation(view, true),
+                                          width: containerWidth,
+                                          height: containerHeight)
+
+            // 레이블 위치 조정
+            let labelX: CGFloat = iconImageView.frame.maxX + iconPadding
+            toastLabel.frame = CGRect(x: labelX, y: 5,
+                                      width: toastContainer.frame.size.width - labelX - iconPadding,
+                                      height: 25)
+
+            view.addSubview(toastContainer)
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: .curveEaseOut, animations: {
+                toastContainer.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastContainer.removeFromSuperview()
+            })
+        }
+    
     public func showToast(_ view: UIView, _ message: String, withDuration: Double, delay: Double, bottomPosition: Bool) {
             let toastContainer = UIView(frame: CGRect())
             toastContainer.backgroundColor = UIColor.black.withAlphaComponent(0.6)
