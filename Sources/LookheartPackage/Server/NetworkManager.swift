8a124cd7087f28499703f51b75b754e4a53bc38c
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-19 14:08:23 +0900

    test

--- Sources/LookheartPackage/Server/NetworkManager.swift
+++ Sources/LookheartPackage/Server/NetworkManager.swift
@@ -78,7 +78,6 @@ public class NetworkManager {
                     
                     // 첫 번째 프로필을 기본 프로필로 설정하고, 핸드폰 번호 목록을 저장
                     if let primaryProfile = userProfiles.first {
-                        UserProfileManager.shared.setUserProfile(primaryProfile)
                         UserProfileManager.shared.setPhoneNumbers(phoneNumbers)
                         completion(.success(primaryProfile))
                     } else {
