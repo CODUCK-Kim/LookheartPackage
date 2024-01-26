50891a274043af1ab13c21a1072cdcd171bb1e92
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-26 13:33:00 +0900

    test

--- Sources/LookheartPackage/Server/NetworkManager.swift
+++ Sources/LookheartPackage/Server/NetworkManager.swift
@@ -43,9 +43,6 @@ public class NetworkManager {
         request(url: url, method: .get, parameters: params) { result in
             switch result {
             case .success(let data):
-                if let responseString = String(data: data, encoding: .utf8) {
-                    print(responseString)
-                }
                 do {
                     let email = try JSONDecoder().decode([Email].self, from: data) // 디코딩
                     completion(.success(email[0].eq))
@@ -86,7 +83,7 @@ public class NetworkManager {
     }
     
     // MARK: - SMS
-    public func sendSMS(phoneNumber: String, nationalCode: String,completion: @escaping (Result<String, Error>) -> Void) {
+    public func sendSMS(phoneNumber: String, nationalCode: String,completion: @escaping (Result<Bool, Error>) -> Void) {
     
         let endpoint = "/mslSMS/sendSMS"
         guard let url = URL(string: baseURL + endpoint) else {
@@ -103,7 +100,13 @@ public class NetworkManager {
             switch result {
             case .success(let data):
                 if let responseString = String(data: data, encoding: .utf8) {
-                    completion(.success(responseString))
+                    
+                    if responseString.contains("true") {
+                        completion(.success(true))
+                    } else {
+                        completion(.failure(NetworkError.invalidResponse))
+                    }
+                    
                 } else {
                     completion(.failure(NetworkError.invalidResponse))
                 }
