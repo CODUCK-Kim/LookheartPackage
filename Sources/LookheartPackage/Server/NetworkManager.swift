eb5db1964ac2f89f2750fde79c6c9eaee2f685c6
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-01-30 16:58:21 +0900

    test

--- Sources/LookheartPackage/Server/NetworkManager.swift
+++ Sources/LookheartPackage/Server/NetworkManager.swift
@@ -503,7 +503,6 @@ public class NetworkManager {
             case .success(let data):
                 if let _ = String(data: data, encoding: .utf8) {
 //                    print("HourlyData Received response: \(responseString)")
-                    print("sendHourlyDataToServer check")
                 }
             case .failure(let error):
                 print("sendHourlyDataToServer Send Error: \(error.localizedDescription)")
