84b8dabb6d09feb1405e8e2edef228df6f0d8b5a
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-02-02 10:39:44 +0900

    test

--- Sources/LookheartPackage/Alert/Alert.swift
+++ Sources/LookheartPackage/Alert/Alert.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 public class MyAlert {
-    // Test
     public static let shared = MyAlert()
     
     public init() {}
@@ -9,6 +8,15 @@ public class MyAlert {
     public func basicAlert(title: String, message: String, ok: String, viewController: UIViewController) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let complite = UIAlertAction(title: ok, style: .default)
+        alert.addAction(complite)
+        viewController.present(alert, animated: true, completion: {})
+    }
+    
+    public func basicAction(title: String, message: String, ok: String, viewController: UIViewController, completion: @escaping () -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let complite = UIAlertAction(title: ok, style: .default) { _ in
+            completion()
+        }
         alert.addAction(complite)
         viewController.present(alert, animated: true, completion: {})
     }
