a2b1f2287e4aa818ee7fbd3ef396785da764edf5
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-02-07 09:44:23 +0900

    test

--- Sources/LookheartPackage/Alert/EmergencyAlert.swift
+++ Sources/LookheartPackage/Alert/EmergencyAlert.swift
@@ -173,7 +173,7 @@ public class EmergencyAlert: UIViewController {
         actionButton.snp.makeConstraints { make in
             make.centerX.equalTo(backgroundView)
             make.bottom.equalTo(backgroundView).offset(-10)
-            make.width.equalTo(80)
+            make.width.equalTo(100)
             make.height.equalTo(35)
         }
 
