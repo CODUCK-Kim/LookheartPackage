46e1780d521f93ad79895835d8971d813f2de607
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-04-08 17:01:16 +0900

    test

--- Sources/LookheartPackage/Server/NetworkError.swift
+++ Sources/LookheartPackage/Server/NetworkError.swift
@@ -2,6 +2,10 @@ import Foundation
 
 
 public enum NetworkError: Error {
+    case success
+    case failer
+    case notConnected
+    case session
     case invalidResponse
     case noData
 }
@@ -10,13 +14,15 @@ public class NetworkErrorManager {
     
     public static let shared = NetworkErrorManager()
     
-    public func getErrorMessage(_ error: NetworkError) -> String {
+    public func getErrorMessage(_ error: NetworkError) -> String? {
         switch (error) {
             
         case .invalidResponse:
             return "serverErr".localized()
         case .noData:
             return "noData".localized()
+        default:
+            return nil
         }
     }
     
