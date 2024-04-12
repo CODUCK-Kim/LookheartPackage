bfa795cb5af3b38e15eb9b129e44f228a980e47b
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-04-12 15:45:36 +0900

    test

--- Sources/LookheartPackage/Repositories/PostData.swift
+++ Sources/LookheartPackage/Repositories/PostData.swift
@@ -55,6 +55,7 @@ public class PostData {
         
         params.merge(tenSecondData) { (current, _) in current }
         
+        print(params)
         do {
             let tenSecondData = try await AlamofireController.shared.alamofireControllerForString(
                 parameters: params,
