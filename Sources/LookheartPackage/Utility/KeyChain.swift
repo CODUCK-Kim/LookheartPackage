584d48484920ceb090fddf88428bafc0ccbd88b9
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-04-29 17:36:43 +0900

    tttt

--- Sources/LookheartPackage/Utility/KeyChain.swift
+++ Sources/LookheartPackage/Utility/KeyChain.swift
@@ -13,27 +13,27 @@ public class Keychain {
     
     private let keychain = KeychainSwift()
      
-    func setString(_ value: String, forKey key: String) {
+    public func setString(_ value: String, forKey key: String) {
         keychain.set(value, forKey: key)
     }
     
-    func getString(forKey key: String) -> String? {
+    public func getString(forKey key: String) -> String? {
         return keychain.get(key)
     }
     
-    func setBool(_ value: Bool, forKey key: String) {
+    public func setBool(_ value: Bool, forKey key: String) {
         keychain.set(value, forKey: key)
     }
     
-    func getBool(forKey key: String) -> Bool {
+    public func getBool(forKey key: String) -> Bool {
         return keychain.getBool(key) ?? false
     }
     
-    func deleteString(forKey key: String) -> Bool {
+    public func deleteString(forKey key: String) -> Bool {
         return keychain.delete(key)
     }
     
-    func clear() -> Bool {
+    public func clear() -> Bool {
         return keychain.clear()
     }
 }
