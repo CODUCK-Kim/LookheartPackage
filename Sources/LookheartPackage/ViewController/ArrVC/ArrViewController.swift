d5b8511ad42c93f9dda4cb5a1577fe6de37b6a2d
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-04-30 15:15:12 +0900

    update

--- Sources/LookheartPackage/ViewController/ArrVC/ArrViewController.swift
+++ Sources/LookheartPackage/ViewController/ArrVC/ArrViewController.swift
@@ -280,11 +280,13 @@ public class ArrViewController : UIViewController {
     func selectArrData(_ startDate: String) {
         activityIndicator.startAnimating()
         
+        print(startDate)
         Task {
             let getArrData = await arrService.getArrData(startDate: startDate)
             let data = getArrData.0
             let response = getArrData.1
             
+            print(getArrData)
             switch response {
             case .success:
                 self.arrChart(data!)
