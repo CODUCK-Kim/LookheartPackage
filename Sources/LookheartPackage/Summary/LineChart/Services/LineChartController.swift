9a02697deb7efe461be9139737eba637b2bfd0d4
Kim Hyun Jin <wlalsl4267@gmail.com>
2024-11-08 17:57:00 +0900

    test

--- Sources/LookheartPackage/Summary/LineChart/Services/LineChartController.swift
+++ Sources/LookheartPackage/Summary/LineChart/Services/LineChartController.swift
@@ -221,8 +221,8 @@ class LineChartController {
             let topLimitLine = model.avgValue + model.standardDeviationValue
             let bottomLimitLine = model.avgValue - model.standardDeviationValue
             
-            addLimitLine(to: lineChart, limit: model.avgValue, label: "unit_standard_deviation".localized(), color: NSUIColor.MY_ORANGE)
-            addLimitLine(to: lineChart, limit: topLimitLine, label: "unit_avg_cap".localized(), color: NSUIColor.MY_RED)
+            addLimitLine(to: lineChart, limit: model.avgValue, label: "unit_avg_cap".localized(), color: NSUIColor.MY_ORANGE)
+            addLimitLine(to: lineChart, limit: topLimitLine, label: "unit_standard_deviation".localized(), color: NSUIColor.MY_RED)
             addLimitLine(to: lineChart, limit: bottomLimitLine, label: "unit_standard_deviation".localized(), color: NSUIColor.MY_RED)
         case .STRESS:
             addLimitLine(to: lineChart, limit: 60, label: "", color: NSUIColor.MY_SKY)
